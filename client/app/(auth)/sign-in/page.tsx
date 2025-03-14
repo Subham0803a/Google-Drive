7195da0eaf82658d75a7a56ef66116e0cc5fd91e
subham <subhampratap0@.com>
2025-03-14 19:50:08 +0530

    Added sign-in and sign-up page

---
+++ client/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
+// const SignIn = () => <AuthForm type="sign-in" />;
+
+// export default SignIn;
+ 
+export default function SignIn() {
+  return (
+      <AuthForm type="sign-in" />
+  )
+}
---
+++ client/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
+// const SignUp = () => <AuthForm type="sign-up" />;
+
+// export default SignUp;
+
+export default function SignUp() {
+  return (
+    <AuthForm type="sign-up" />
+  )
+}
